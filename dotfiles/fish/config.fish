1341df5cf6d17d1920ff1beb13a420b380d36e3d
Prince Junior Mguni <princem@bestforu.co.za>
2024-05-10 09:51:30 +0200

    added fast fetch

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fastfetch
     #Fish Settings
     set fish_greeting
 
--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@ let
     hugo
     helix
     lsof
+    fastfetch
     go-swag
     tailwindcss
     templ
