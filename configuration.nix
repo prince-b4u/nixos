e1f6ab5c663eb7cedaccb8bce7b10345900f0d7e
Prince Junior Mguni <princem@bestforu.co.za>
2024-06-26 12:50:11 +0200

    removed java

--- configuration.nix
+++ configuration.nix
@@ -36,10 +36,6 @@ programs.fish = {
   enable = true;
 };
 
-programs.java = {
-  enable = true;
-};
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
